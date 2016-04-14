de652d518a0c7a1290305e9da2737bacca1c819d
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-14 11:43:06 -0600

    adding more code have gotter persona done and use cases

--- index.php
+++ index.php
@@ -1,7 +1,61 @@
 <!DOCTYPE html>
-	<html>
+<html xmlns="http://www.w3.org/1999/html">
 	<head>
+		<meta charset="utf-8"/>
 		<title> data design for Amazon review</title>
 	</head>
-	<body> </body>
+	<body>
+		<h2>Persona</h2>
+		<img src="images/imgres.jpg" alt="pic of a mom" border="2px"/>
+			<p> <strong>Personal</strong></p>
+				<ul>
+					<li>name: Kimberly Bacon</li>
+					<li>Age: 35 years old</li>
+					<li>Gender: Female</li>
+					<li>highest level of education: Highschool deploma</li>
+					<li>tech savvy</li>
+					<li>blogger</li>
+					<li>loves writing reviews</li>
+					<li>loves fitness (mainly yoga)</li>
+				</ul>
+			<p> <strong> Professional life</strong></p>
+				<ul>
+					<li>profession:stay at home mom</li>
+					<li>why will user come to site: to write a review for product</li>
+					<li>when will user access site: after a purchase to list review</li>
+				</ul>
+			<p> <strong>Technical</strong></p>
+				<ul>
+					<li>Tech devices used: Tablet, cellphone, Laptop</li>
+					<li>time spent browsing web: a lot more than 8 hours a day</li>
+					<li>Softwares & apps used: <strong>WINDOWS</strong>, Android, Google Maps, Etsy </li>
+				</ul>
+			<p><strong>Motivations</strong></p>
+				<ul>
+					<li>Child needs and happiness, wanting child to be smart </li>
+					<li>Looking for: Tech and ways to make things faster and easier for busy mom</li>
+				</ul>
+				<hr/>
+		<h2>Use Casees</h2>
+			<ul>
+				<li>Use case 1: user writes a review by selecting product purchased, then selecting "write review", writing review, giving a star rating rating then clicking post</li>
+				<li>Use case 2: User reviews reviews by searching product interested in, finding and reading reviews, then selecting rather they were helpful or not via a thumbs up or down button.  </li>
+			</ul>
+
+			<hr/>
+		<h2>Entities</h2>
+			<h3>User id</h3>
+				<li>Review</li>
+				<li>star rating(1-5)</li>
+				<li>helpful or not button</li>
+			</ul>
+		<hr/>
+		<h1>Conceptual Model</h1>
+			<p><strong>relationships</strong></p>
+				<ul>
+					<li><em>one</em> <strong>User</strong> can post a<strong>Review</strong> for <em>many</em> products</li>
+					<li><em>one</em> <strong>User</strong> can give <strong>star ratings</strong>to <em>many</em>products</li>
+					<li><em></em></li>
+				</ul>
+	</body>
 </html>
