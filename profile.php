b3c10998620a75cafa4aa6e8e0f1f60f500ebdd8
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-18 23:45:11 -0600

    added profile.php and started coding it

---
+++ profile.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Profile for an Amazon review
+ *
+ * This Profile is information that is collect by Amazon when an account is created.
+ *
+ * @author Elliot Murrey <emurrey@cnm.edu>
+ **/
+class Profile {
+	/**
+	 * id for this Profile; this is the primary key
+	 **/
+	private $profileId;
+	/**
+	 * id for the user who owns this Profile; this is a foreign key
+	 **/
+	private $userId;
+	/**
+	 *
+	 **/
+}
+?>
