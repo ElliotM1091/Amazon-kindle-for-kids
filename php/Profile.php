c950f1ff17ade4f7b881fc451da2545da046a099
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-21 11:52:22 -0600

    still doing accessor and mutators have 6 or 7 to do total

--- php/Profile.php
+++ php/Profile.php
@@ -63,12 +63,65 @@ class Profile implements \JsonSerializeable {
 	/**
 	 * mutator method for profile id
 	 *
-	 * @param int new value of profile id
-	 * @throws UnexpectedValueException if $newProfileId is not an interger
+	 * @param int $newProfileId new value of profile id
+	 * @throws \UnexpectedValueException if $newProfileId is not an interger
 	 **/
 	public function setProfileId($newProfileID) {
-		$newProfileId = filter var(newProfileId)
-
+		//verify the profile id is valid
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		if($newProfileId === false) {
+			throw(new \UnexpectedValueException("profile id is not a valid integer"));
+		}
+		//convert and store the profile id
+		$this->profileId = intval($newProfileId);
 	}
+	/**
+	 * accessor method for activities
+	 *
+	 * @return string value of activities
+	 **/
+	public function getActivities() {
+		return($this->activities);
+	}
+	/**
+	 * mutator method for activities
+	 *
+	 * @param string $newActivities new value of activities
+	 * @throws \InvalidArgumentException if $newactivities is not a string
+	 * @throws \TypeError if $newActivities is not a string
+	 **/
+	public function setActivities(string $newactivities) {
+		//verify the activities content is secure
+		$newActivities = trim($newActivities);
+		$newActivities = filter_var($newActivities, FILTER_SANITIZE_STRING);
+		//store the activities content
+		$this->activities = $newActivities;
+	}
+	/**
+	 * accessor method for email
+	 *
+	 * @return string value of email
+	 **/
+	public function getemail() {
+		return($this->email);
+	}
+	/**
+	 * mutator method for email
+	 * @param sting $newEmail new
+	 * @thows \UnexpectedValueException if $newEmail is not valid
+	 **/
+	public function setEmail($newEmail) {
+		// verify that the email is valid
+		$newEmail = filter_var($newEmail, FILTER_SANITIZE_STRING);
+		if($newEmail ===false) {
+			throw(new \UnexpectedValueException("Email is not valid"));
+		}
+		//store the emial
+		$this->email = $newEmail;
+	}
+	/**
+	 * accessor method following
+	 * @return 
+	 **/
 }
 ?>
