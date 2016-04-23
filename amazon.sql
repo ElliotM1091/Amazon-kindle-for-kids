514e12f53baa1efca33c8ad41d56ccf9a1d5105d
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-23 17:00:12 -0600

    finished 1st table adding second now

--- amazon.sql
+++ amazon.sql
@@ -3,11 +3,16 @@ DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
-	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	profileName VARCHAR (32) NOT NULL,
-	profileEmail VARCHAR (128) NOT NULL,
-	profileAvatar VARCHAR (32),
-	profileFollow VARCHAR (32),
+	profileId            INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileName          VARCHAR(32)                 NOT NULL,
+	profileEmail         VARCHAR(128)                NOT NULL,
+	profileAvatar        VARCHAR(32),
+	profileFollow        VARCHAR(32),
 	profileHelpfullVotes INT UNSIGNED,
-	profileActivities
+	profileActivities    VARCHAR(2000)
+		UNIQUE (email)
+);
+CREATE TABLE review (
+	reviewId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	Tittle VARCHAR(32) NOT NULL
 )
